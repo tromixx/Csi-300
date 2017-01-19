ddd542f3a8b6e571079514a0805da6cea8076a85
tromixx <veneret@gmail.com>
2017-01-19 10:50:20 -0500

    Update Csi.200.HelloWorld.java

--- Csi.200.HelloWorld.java
+++ Csi.200.HelloWorld.java
@@ -1,5 +1,11 @@
 public class Csi.200.HelloWorld.java
 {
+  public static void main(String[] args)
+    {
+    System.out.println("Hello world!!!");
+    System.out.println("Tomas Venere");
+    
+    }
   
 
 }
