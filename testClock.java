6a891e732b7b992782845578bb1bab9ca107555c
tromixx <veneret@gmail.com>
2017-01-19 11:39:05 -0500

    Update testClock.java

--- testClock.java
+++ testClock.java
@@ -8,18 +8,21 @@ public class testClock
   public static void main(String[] args)
   {
     //getting input from keyboard
-    Scanner scan = new Scanner(system.in)
+    Scanner scan= new Scanner(system.in)
     
     
     //prompt user to enter a time
     System.out.println("Enter hours: ");
-    int hours = scan.nextInt();
+    int hours= scan.nextInt();
     
     System.out.println("Enter hours: ");
-    int minutes = scan.nextInt();
+    int minutes= scan.nextInt();
     
     System.out.println("Enter hours: ");
-    int seconds = scan.nextInt();
+    int seconds= scan.nextInt();
+    
+    //test constructor
+    Clock time= new Clock(hours, minutes, seconds);
   }
 
 }
