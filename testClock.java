289b79300d971b304578e49c34be8a3231efbcab
tromixx <veneret@gmail.com>
2017-01-19 11:35:09 -0500

    Create testClock.java

---
+++ testClock.java
@@ -0,0 +1,25 @@
+
+
+
+import java.util.Scanner;
+
+public class testClock
+{
+  public static void main(String[] args)
+  {
+    //getting input from keyboard
+    Scanner scan = new Scanner(system.in)
+    
+    
+    //prompt user to enter a time
+    System.out.println("Enter hours: ");
+    int hours = scan.nextInt();
+    
+    System.out.println("Enter hours: ");
+    int minutes = scan.nextInt();
+    
+    System.out.println("Enter hours: ");
+    int seconds = scan.nextInt();
+  }
+
+}
