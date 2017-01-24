a29cad59926d8d1490cd5887ad7f087af3b29738
tromixx <veneret@gmail.com>
2017-01-24 11:50:27 -0500

    Update PhoneDirectory.java

--- PhoneDirectory.java
+++ PhoneDirectory.java
@@ -1,8 +1,26 @@
 import java.util.Scanner;
 
 public class PhoneDirectory
-{
-  {
+{  
   public static void main(String [] args)
-  }
+  {
+    PhoneRecord [] records = new PhoneRecord[100];
+    //total number of records in the array
+    int numRecords = 0;
+    
+    //Display list of commands
+    System.our.println("Phone directory commands: \n" +
+                      "a- Add a new phone number\n" +
+                      "f - Find a phone number\n" +
+                      "q - Quit\n");
+    
+    
+    
+    
+    
+    
+    
+    
+  }// end of main
+  
 }
