cd8e89f541941ced6c350619537739d5c4d82771
tromixx <veneret@gmail.com>
2017-01-24 11:44:50 -0500

    Create PhoneRecord.java

---
+++ PhoneRecord.java
@@ -0,0 +1,31 @@
+/*
+Ex2
+Tomas Venere
+Date
+Represents a record containing a name and a phone number
+/*
+
+public class PhoneRecord 
+{
+  private String name;
+  private String number;
+
+  //Constructor
+  public PhoneRecord(String personName, String phoneNumber)
+  {
+    name= personName;
+    number= phoneNumber;
+  }
+  
+  //Return the name stored in the record
+  public String getName()
+  {
+    return name
+  }
+  
+  //Return the name stored in the record
+  public String getName()
+  {
+    return name
+  }
+}
