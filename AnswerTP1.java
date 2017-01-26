52de67721e6a8951caf5205705aedb2d86025081
tromixx <veneret@gmail.com>
2017-01-26 11:27:34 -0500

    Update AnswerTP1.java

--- AnswerTP1.java
+++ AnswerTP1.java
@@ -26,6 +26,9 @@ public class 1
   int a = 538;
   int res1 = countDigit(a);
   System.out.println(a + "numDigits:"+ res1);
+    
+  double [] array1 = {3, 0, 3, 4, 5}
+    System.out.println("sumArray(array: +" )
   
   }
   
@@ -52,13 +55,44 @@ public class 1
   
   
   
-  private double sumArray(double [] values)
+  private static double sumArray(double [] values)
   {
     double sum= 0;
     fo(int index = 0; index <= values.length; index==)
     {
+      sum+= values[index];
+    }
+    return sum;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+private static void swichRhem(int[] first, int[] second)
+{
+  if(first.length == second.length)
+  {
+    for(int i= 0; i < first.length; i++)
+    {
+        int temp= first[i];
+        first[i] = second[i];
+        second[i] = temp;
     }
   }
+  else
+  {
+      System.out.println("Arrays are of different sizes");
+  }
+}
   
   
   
