3b4469d0dbd9753260b83d17b22b34d02a2f0229
tromixx <veneret@gmail.com>
2017-01-26 11:50:01 -0500

    Update PhoneDirectory.java

--- PhoneDirectory.java
+++ PhoneDirectory.java
@@ -22,6 +22,45 @@ public class PhoneDirectory
     
     
     
+    //read and execut commands
+    while(true)
+    {
+      //Prompt user to enter a command
+      System.out.println("Enter command (a, f or q): ");
+      String command = scan1.next().trim();
+      
+      
+      //Determine wether command is a f q or illegal; execute command if legal
+      if(command.equalsIgnoreCase( anotherString: "a"))
+      {
+        
+        //homework: fix this problem if the database is full (what happend if you have 101)!
+        
+        //prompt user for name and number, create a phone record and store it in database
+        System.out.println("Enter new name: ");
+        String name= scan2.nextLine().trim();
+        System.out.println("Enter new phone number: ");
+        String number = scan2.nextLine().trim();
+        
+        //numRecords tell you the next available position
+        records[numRecords] = new PhoneRecords(name, number);
+        numRecords++;
+        
+        
+        
+        
+        
+        
+      }
+      
+      
+      
+      
+    }
+    
+    
+    
+    
     
   }// end of main
   
