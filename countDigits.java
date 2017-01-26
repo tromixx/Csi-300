d0b8671e62fb5b337c2b55a1c8a6bc2d65c44285
tromixx <veneret@gmail.com>
2017-01-26 09:26:25 -0500

    Update and rename countDigits to countDigits.java

---
+++ countDigits.java
@@ -0,0 +1,18 @@
+//cuenta la cantidad de digitos que tiene un num y devuelve el num. Ej 12342 seria 5
+
+public class countDigits
+{
+  public static void main(String[] args)
+  {
+  
+  
+  
+  
+  }
+
+
+}
+
+
+
+//int length = String.valueOf(X).length();
