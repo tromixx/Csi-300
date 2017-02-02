5a80dcfe970a8087e2d7fc4897746a21f950eff2
tromixx <veneret@gmail.com>
2017-02-02 11:38:23 -0500

    Update Ex4.java

--- Ex4.java
+++ Ex4.java
@@ -3,4 +3,31 @@
 //
 //
 public class Account {
+  privatedouble balance;
+  
+  //Constructor
+  public Account(double initialBalance){
+    balance = initialBalance;
+  }
+  
+  public Account()
+  {
+    balance= 0.0;
+  
+  }
+  
+  //instance method
+  public void deposit(double amount)
+  {
+    balance += amount;
+    
+  }
+  public void close()
+  {
+    balance= 0;
+  }
+  public void printStatment()
+  {
+    System.out.println(balance);
+  }
 }
