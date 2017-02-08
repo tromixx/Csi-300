e969f27919ff2d2b0bb44f8e042907cbd5db8005
tromixx <veneret@gmail.com>
2017-02-08 10:50:20 -0500

    Create Name.java

---
+++ Name.java
@@ -0,0 +1,11 @@
+import util.Scanner
+public class Name.java
+{
+  public static void main(String[] args)
+    {
+    Scanner scan = new Scanner(System.in);
+    firstName= scan.nextline
+    Scanner scan1 = new Scanner(System.in);
+    lastName= scan1.nextline
+    }
+}
