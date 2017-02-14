83c342285d6bcca706931527e9ea87167348b2c0
tromixx <veneret@gmail.com>
2017-02-14 11:38:19 -0500

    Update TestSavingAccount.java

--- TestSavingAccount.java
+++ TestSavingAccount.java
@@ -3,8 +3,15 @@
 public class TestSavingAccount 
  {
   public static void main(String[] args)
-    {
-    
+  {
+    SavingsAccount savingAcct = new SavingsAccount( initialBalance: 100, initialRate:0.1);
+   
+    System.out.println("Rate: " = savingAcct.getInterestRate());
+   
+    savingAcct.setInterestRate(0.5);
+   
+    savingAcct.deposit(amount: 500.0);
+  }
     
     
     
