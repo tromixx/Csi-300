d32bd345a5402261f7cad7c6b03f8133e52928ae
tromixx <veneret@gmail.com>
2017-02-20 10:23:55 -0500

    Create CSI200NombreYApellido.java

---
+++ CSI200NombreYApellido.java
@@ -0,0 +1,20 @@
+//Initial Lab from class on 2/20
+//Write a program that requests a users first and last name.  Use
+//String methods to pull out and display the person's initials to the
+//Screen.  Ex.  nicole tobias  should produce the result N. T.
+
+import java.util.Scanner;
+
+public class CSI200NombreYApellido
+{
+   public static void main(String[] args)
+   {
+      String fName, lName, initials;
+      Scanner scan = new Scanner(System.in);
+      
+      System.out.print("Please enter your name:  ");
+
+      
+      System.out.println("Your initials are:  " + initials);  
+   }
+}
