a51083830f7bff2a6b8afaeea4c4f78b7bd55243
tromixx <veneret@gmail.com>
2017-02-28 11:13:00 -0500

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,2 +1,24 @@
 public class Employee extends Person    //Employee is a Person
     Private double Salary;
+    Priva Date employmentDate;
+    public Employee( String mylastName,
+                String myFirstName,Date my BirthDate,
+                double mySalery, Date myEmploymentDate)
+    {
+        call constructor of superclass with three parameter
+            super(myLastName, myFirsName, myBirthDate);
+        salery= mySalery;
+        date= myDate;
+    }
+
+    public double getSalery()
+    {
+        return salery;
+    }
+
+    public Date getEmployeementDate()
+    {
+        return employmentDate;
+    }
+    public void setSalery(double mySalery)
+        //...
