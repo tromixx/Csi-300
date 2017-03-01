bc079ce25f0898b8cf9542010a37cf861ccf0eb4
tromixx <veneret@gmail.com>
2017-03-01 10:15:08 -0500

    Update x_and_y.java

--- x_and_y.java
+++ x_and_y.java
@@ -1,4 +1,11 @@
 public class x_and_y
 {
-
+  //instance variables (attributes)
+  private double x;
+  private double y;
+  
+  //method (operations)
+  //Constructors (we can have more than 1, always need to have the same name of the class)
+  
+  
 }
