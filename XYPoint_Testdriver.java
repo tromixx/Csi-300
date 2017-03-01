7f72d92cd2a814d237eaf94ca11913998e2f30f3
tromixx <veneret@gmail.com>
2017-03-01 10:22:12 -0500

    Update XYPoint_Testdriver.java

--- XYPoint_Testdriver.java
+++ XYPoint_Testdriver.java
@@ -3,7 +3,10 @@ public class XYPoint_Testdriver.java
   public static void main(String[] args)
   {
   //create two point objects
-  XYpoint
+  XYpoint pt1= new XYpoint();
+  
+  //print
+  System.out.println( pt1 );
   }
 
 
