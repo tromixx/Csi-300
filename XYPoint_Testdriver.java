aff887f10bea7617eb4b7669b159348e06f39257
tromixx <veneret@gmail.com>
2017-03-05 10:19:33 -0500

    Update XYPoint_Testdriver.java

--- XYPoint_Testdriver.java
+++ XYPoint_Testdriver.java
@@ -4,6 +4,12 @@ public class XYPoint_Testdriver.java
   {
   //create two point objects
   XYpoint pt1= new XYpoint();
+  System.out.println( pt1 );
+  pt1.setXY (7.5, 8.1);
+  System.out.println( p1 );
+  pt1.setX(-7.25);
+  System.out.println (p1);
+
   
   //print
   System.out.println( pt1 );
