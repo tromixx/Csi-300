a9fdc2c75ce608b62532cb3ba8f8cef1dc090245
tromixx <veneret@gmail.com>
2017-03-13 10:44:31 -0400

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -6,9 +6,34 @@ public class Circle
     private String color;
     
     //Constructor
+    public Circle()
+    {
+    }
+    public Circle(double r)
+    {
+    }
+    public Circle (double r, String c)
+    {
+    }
     
     //Method
-  
+    public double getRadius()
+    {
+     return 0.0;
+     
+    }
+    public double
+    {
+     return 0.0;
+    }
+    public void setColor(String c)
+    {
+     
+    }
+    public String toString()
+    {
+     return "";
+    }
   
   
   
