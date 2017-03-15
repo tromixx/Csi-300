e3acebc5d5375789ad158c7976231356a9c18a93
tromixx <veneret@gmail.com>
2017-03-15 10:19:19 -0400

    Update Cat.java

--- Cat.java
+++ Cat.java
@@ -1,2 +1,31 @@
-import 
+import java.util.Scanner;
+
 public class Cat
+{
+  public static void main(String[] args)
+  {
+   double weight= 0.0;
+   String mood= "QUIET";
+   
+   
+   Scanner scan= new Scanner(System.in);
+   
+    If (scan= "eat")
+      weight+= 1;
+    
+    If (scan= "poop")
+      weight-= 1;
+    
+    If (scan= "kick")
+      mood= "MEOW!";
+      
+    If (scan= "pet")
+      mood= "PURRR"
+   
+    If (scan= "toString")
+      System.out.println("The cat is currently in a "+ mood+ " mood and weighs "+ weight+ " lbs")
+    
+  }
+  
+  
+}
