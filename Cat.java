b43de15006aa00bb26ad23192e6f13ba5ac4f177
tromixx <veneret@gmail.com>
2017-03-16 10:21:07 -0400

    Update Cat.java

--- Cat.java
+++ Cat.java
@@ -30,4 +30,48 @@ public class Cat
   
 }
 
-public class  
+public class fluffy
+{
+  public static void main(String[] args)
+  {
+    weight= 10;
+    mood= "QUIET";
+  }
+}
+
+
+
+privateString mood
+
+//Constructor
+public Cat()
+{
+  weight= 0.0;
+  mood= "QUIET";
+  
+}
+public class (double w, String m)
+{
+  
+}
+
+public void eat()
+{
+  weight++;
+}
+  public void poop()
+  {
+    weight--;
+  }
+
+  public void kick()
+    
+  public void pet()
+  public void ()
+  public void ()...
+    
+    public double getWeight ()
+  {
+   return= weight; 
+  }
+  
