59caf91bcdb5e3e8ad548b072c3edb81b5094b35
tromixx <veneret@gmail.com>
2017-03-29 10:28:58 -0400

    Update switch.java

--- switch.java
+++ switch.java
@@ -2,5 +2,13 @@ import java.util.Scanner;
 public class switchEx
 {
    public static void main(String[] args)
+   {
+      System.out.println("The break statement does wich of the following"+
+                        "\n\t a) ends the program"+
+                        "\n\t b) transfer control out of the current control"+
+                        "\n\t c) ends the current line of output, returning the cursor"+
+                        "\n\t d) denotes the ending of switch statment"+
+                        "\n\t e) indicates the end of the line when using System.out.println")
+   }
 
 }
