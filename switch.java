07b522e4161be1a22ca4ffebcb072ca55d2c6c52
tromixx <veneret@gmail.com>
2017-03-29 10:51:39 -0400

    Update switch.java

--- switch.java
+++ switch.java
@@ -9,6 +9,20 @@ public class switchEx
                         "\n\t c) ends the current line of output, returning the cursor"+
                         "\n\t d) denotes the ending of switch statment"+
                         "\n\t e) indicates the end of the line when using System.out.println")
+         system.out.print("Please enter your answer");
+         Scanner scan = new Scanner(System.in)
+         char response = scan.next()charAt(0)
+            
+            switch(response)
+            {
+               case 'a': System.out.println("incorrect!");
+               case 'b': System.out.println("incorrect!");
+               case 'c': System.out.println("incorrect!");
+               case 'd': System.out.println("incorrect!");
+               case 'e': System.out.println("incorrect!");
+               default: System.out.println("Invalid response, you idiot!");     
+                   
+            }
    }
 
 }
